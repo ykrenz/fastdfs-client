ce1d1022b332deca487311800f0840040a4002a1
ykren <ykren@trip.com>
2022-02-09 20:33:48 +0800

    fix pool

--- src/main/java/com/ykren/fastdfs/FastDFSConfiguration.java
+++ src/main/java/com/ykren/fastdfs/FastDFSConfiguration.java
@@ -61,69 +61,38 @@ public class FastDFSConfiguration {
     }
 
     protected static class Pool extends GenericKeyedObjectPoolConfig {
+        /**
+         * 获取连接时的最大等待毫秒数 5s
+         */
+        private static final long MAX_WAIT_MILLIS = 5000;
+        /**
+         * 每个key最大连接数 500
+         */
+        private static final int MAX_TOTAL_PER_KEY = 500;
+        /**
+         * 每个key最小空闲连接数 10
+         */
+        private static final int MIN_IDLE_PER_KEY = 10;
+        /**
+         * 每个key最大空闲连接数 100
+         */
+        private static final int MAX_IDLE_PER_KEY = 100;
+        /**
+         * 空闲连接存活时长 30min
+         */
+        private static final long IDLE_TIME_MILLIS = 1000L * 60L * 30L;
+        /**
+         * 清理空闲连接任务时长 1min
+         */
+        private static final long EVICT_IDLE_SCHEDULE_TIME_MILLIS = 1000L * 60L;
 
         public Pool() {
-            setMaxWaitMillis(getMaxWaitMillis());
-            setMaxTotalPerKey(getMaxTotalPerKey());
-            setMaxIdlePerKey(getMaxIdlePerKey());
-            setMinIdlePerKey(getMinIdlePerKey());
-        }
-
-        /**
-         * 获取连接时的最大等待毫秒数(默认配置为5秒)
-         */
-        private long maxWaitMillis = 5000;
-        /**
-         * 每个key最大连接数
-         */
-        private int maxTotalPerKey = 200;
-        /**
-         * 每个key最小空闲连接数
-         */
-        private int minIdlePerKey = 10;
-        /**
-         * 每个key最大空闲连接数
-         */
-        private int maxIdlePerKey = 100;
-
-        @Override
-        public long getMaxWaitMillis() {
-            return maxWaitMillis;
-        }
-
-        @Override
-        public void setMaxWaitMillis(long maxWaitMillis) {
-            this.maxWaitMillis = maxWaitMillis;
-        }
-
-        @Override
-        public int getMaxTotalPerKey() {
-            return maxTotalPerKey;
-        }
-
-        @Override
-        public void setMaxTotalPerKey(int maxTotalPerKey) {
-            this.maxTotalPerKey = maxTotalPerKey;
-        }
-
-        @Override
-        public int getMinIdlePerKey() {
-            return minIdlePerKey;
-        }
-
-        @Override
-        public void setMinIdlePerKey(int minIdlePerKey) {
-            this.minIdlePerKey = minIdlePerKey;
-        }
-
-        @Override
-        public int getMaxIdlePerKey() {
-            return maxIdlePerKey;
-        }
-
-        @Override
-        public void setMaxIdlePerKey(int maxIdlePerKey) {
-            this.maxIdlePerKey = maxIdlePerKey;
+            setMaxWaitMillis(MAX_WAIT_MILLIS);
+            setMaxTotalPerKey(MAX_TOTAL_PER_KEY);
+            setMaxIdlePerKey(MIN_IDLE_PER_KEY);
+            setMinIdlePerKey(MAX_IDLE_PER_KEY);
+            setMinEvictableIdleTimeMillis(IDLE_TIME_MILLIS);
+            setTimeBetweenEvictionRunsMillis(EVICT_IDLE_SCHEDULE_TIME_MILLIS);
         }
     }
 }
